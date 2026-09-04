260fab6ebd68effae1159016d3f5516cb7b12f9e
dlopez <dlopez@tavanoteam.com>
2026-09-04 12:47:11 -0600

    Updating Hello World message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Tavano Team 123!';
+    return 'Hello Tavano Team 1234!';
   }
 }
